02a3a8def261800e29f82ef6ae541971a311407a
pavan-saaho <pvnklnmalla@gmail.com>
2022-06-19 01:18:45 +0530

    save

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import DatePicker from './components/date-picker/date-picker';
 function App() {
   return (
     <div className="App">
-      <DatePicker
-        placeholder="Select Date"
-      />
+      
     </div>
   );
 }
